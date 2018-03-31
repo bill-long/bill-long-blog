803182a2804133c19ed888ebd5e9e2655868f030
Bill Long <bilong@microsoft.com>
2018-03-31 13:35:16 -0500

    Perform 301 redirect instead of rewrite

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
 
 namespace Web
 {
     public class Startup
     {
+        private static readonly Regex Uppercase = new Regex("[A-Z]");
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -25,7 +30,13 @@ namespace Web
             {
                 if (context.Request.Path.HasValue)
                 {
-                    context.Request.Path = new PathString(context.Request.Path.Value.ToLower());
+                    if (Uppercase.IsMatch(context.Request.Path))
+                    {
+                        var response = context.Response;
+                        response.StatusCode = StatusCodes.Status301MovedPermanently;
+                        response.Headers[HeaderNames.Location] = context.Request.Path.Value.ToLower();
+                        return;
+                    }
                 }
 
                 await next();
